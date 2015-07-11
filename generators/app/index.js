fd371b6b5531b92d09788af7f9fbc7ff40fcb200
Michael Bullington <mikebullingtn@gmail.com>
2015-07-11 18:19:33 -0400

    Added generator-dslink, used generator-generator as a base.

---
+++ generators/app/index.js
@@ -0,0 +1,75 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the fantastic ' + chalk.red('dslink') + ' generator!'
+    ));
+
+    var prompts = [
+      {
+        type: 'input',
+        name: 'appName',
+        store: true,
+        message: "Name of package? We suggest following a 'dslink-javascript-(name)' convention.",
+        validate: function (val) {
+          return val.length > 0 ? true : 'Please enter an app name.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        store: true,
+        message: 'Description? Something short and simple that describes your link.',
+        validate: function (val) {
+          return val.length > 0 ? true : 'Please enter a description.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'linkName',
+        store: true,
+        message: "Name of the link? This is what you'll see your link as from DSA applications such as DGLux.",
+        validate: function (val) {
+          return val.length > 0 ? true : 'Please enter a link name.';
+        }
+      }
+    ];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+
+      this.fs.copy(
+        this.templatePath('_dslink.json'),
+        this.destinationPath('dslink.json')
+      );
+
+      this.fs.copy(
+        this.templatePath('lib/_index.js'),
+        this.destinationPath('lib/index.js')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
